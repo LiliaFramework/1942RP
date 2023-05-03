6e1fb28637620545e6950fd9cecd0c1bd0be8b78
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-03 20:13:41 +0100

    Almost there

--- gamemodes1/1942rp/plugins/entities/items/entitems/sh_wheel.lua
+++
@@ -1,7 +0,0 @@
-
-ITEM.name = "Wheel"
-ITEM.desc = "A highly valuable part of a vehicle."
-ITEM.price = 20
-ITEM.model = "models/NatesWheel/nateswheelwide.mdl"
-ITEM.entdrop = "vc_pickup_tire"
-ITEM.noBusiness = true
