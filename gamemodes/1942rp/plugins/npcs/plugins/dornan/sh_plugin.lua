96fb9cf65b1a1fbe30384a468efb803ad295e04a
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-04 03:22:21 +0100

    Removed Obsolete Stuff

--- gamemodes/1942rp/plugins/npcs/plugins/dornan/sh_plugin.lua
+++
@@ -1,6 +0,0 @@
-PLUGIN.name = "Example Module"
-PLUGIN.desc = "This Module Adds an Functional Talkable NPC."
-PLUGIN.author = "Leonheart#7476"
-------------------------------------------------------------------------------------------
-lia.util.include("sv_plugin.lua")
-------------------------------------------------------------------------------------------
