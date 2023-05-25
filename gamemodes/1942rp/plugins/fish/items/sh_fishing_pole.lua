c26c494989edc48eb56b46af655e16058b27c152
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-25 01:10:00 +0100

    Tweaked Fishing Pole - Keegan

--- gamemodes/1942rp/plugins/fish/items/sh_fishing_pole.lua
+++ gamemodes/1942rp/plugins/fish/items/sh_fishing_pole.lua
@@ -19,7 +19,7 @@ ITEM.functions.FishBait = {
     onRun = function(item)
         local client = item.player
 
-        if (IsValid(client.FishingHook)) then
+        if IsValid(client.FishingHook) then
             client:notify("You've already cast a hook!")
 
             return false
@@ -38,7 +38,7 @@ ITEM.functions.FishBait = {
         end
 
         timer.Simple(2, function()
-            if (hook:WaterLevel() > 0) then
+            if hook:WaterLevel() > 0 then
                 local inventory = client:getChar():getInv()
                 local bait = inventory:hasItem("fishing_bait")
                 local char = client:getChar()
@@ -50,7 +50,7 @@ ITEM.functions.FishBait = {
                     local position = client:getItemDropPos()
                     item:setData("producing2", 0)
 
-                    if (item ~= nil and client:GetPos():Distance(oldPos) <= 500 and bait) then
+                    if item ~= nil and client:GetPos():Distance(oldPos) <= 500 and bait then
                         local char = client:getChar()
                         local inventory = char:getInv()
                         local position = client:getItemDropPos()
@@ -59,43 +59,43 @@ ITEM.functions.FishBait = {
                         if randnum <= 20 and randnum > 21 then
                             client:notify("You found a(n) " .. "Lake Trout" .. "!")
 
-                            if (not inventory:add("Lake Trout", 1)) then
+                            if not inventory:add("Lake Trout", 1) then
                                 lia.item.spawn("Lake Trout", position)
                             end
                         elseif randnum <= 40 and randnum > 20 then
                             client:notify("You found a(n) " .. "Bull Trout" .. "!")
 
-                            if (not inventory:add("Bull Trout", 1)) then
+                            if not inventory:add("Bull Trout", 1) then
                                 lia.item.spawn("Bull Trout", position)
                             end
                         elseif randnum <= 90 and randnum > 40 then
                             client:notify("You found a(n) " .. "Brown Trout" .. "!")
 
-                            if (not inventory:add("Brown Trout", 1)) then
+                            if not inventory:add("Brown Trout", 1) then
                                 lia.item.spawn("Brown Trout", position)
                             end
                         elseif randnum <= 110 and randnum > 90 then
                             client:notify("You found a(n) " .. "Brook Trout" .. "!")
 
-                            if (not inventory:add("Brook Trout", 1)) then
+                            if not inventory:add("Brook Trout", 1) then
                                 lia.item.spawn("Brook Trout", position)
                             end
                         elseif randnum <= 120 and randnum > 110 then
                             client:notify("You found a(n) " .. "Tiger Trout" .. "!")
 
-                            if (not inventory:add("Tiger Trout", 1)) then
+                            if not inventory:add("Tiger Trout", 1) then
                                 lia.item.spawn("Tiger Trout", position)
                             end
                         elseif randnum <= 130 and randnum > 120 then
                             client:notify("You found a(n) " .. "Rainbow Trout" .. "!")
 
-                            if (not inventory:add("Rainbow Trout", 1)) then
+                            if not inventory:add("Rainbow Trout", 1) then
                                 lia.item.spawn("Rainbow Trout", position)
                             end
                         end
                     end
 
-                    if (math.random(0, 100) < 45) then
+                    if math.random(0, 100) < 45 then
                         client:notify("Your bait was lost.")
                         client:getChar():getInv():remove("fishing_bait")
                     end
@@ -111,11 +111,12 @@ ITEM.functions.FishBait = {
         return false
     end,
     onCanRun = function(item)
+        if IsValid(item.entity) then return false end
         local endTime = item:getData("producing2", 0) + 5
         local player = item.player or item:getOwner()
-        if (not player:getChar():getInv():hasItem("fishing_bait")) then return false end
+        if not player:getChar():getInv():hasItem("fishing_bait") then return false end
 
-        if (CurTime() > endTime or item:getData("producing2", 0) > CurTime() or item:getData("producing2", 0) == 0) then
+        if CurTime() > endTime or item:getData("producing2", 0) > CurTime() or item:getData("producing2", 0) == 0 then
             return true
         else
             return false
