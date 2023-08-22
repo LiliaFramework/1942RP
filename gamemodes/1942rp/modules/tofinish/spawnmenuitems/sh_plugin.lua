f1a675d98e2e3d530506065a4edc037dcbbe2c24
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-08-23 00:01:48 +0100

    Removed useless file

--- gamemodes/1942rp/modules/tofinish/spawnmenuitems/sh_plugin.lua
+++
@@ -1,7 +0,0 @@
-local MODULE = MODULE
-MODULE.name = "Spawn Menu Items"
-MODULE.desc = "Adds a tab to the spawn menu for spawning items."
-MODULE.author = "STEAM_0:1:176123778"
-MODULE.cooldown = 0.5
-lia.util.include("cl_module.lua")
-lia.util.include("sv_module.lua")
