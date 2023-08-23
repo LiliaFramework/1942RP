e8a4ab24ef0716a2230dd86cbf6f99a651e6723f
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-08-23 01:05:24 +0100

    Fixed Bugs

--- gamemodes/1942rp/modules/trunk_storage/sh_module.lua
+++
@@ -1,6 +0,0 @@
-MODULE.vehicles = MODULE.vehicles or {}
-VEHICLE_DEFINITIONS = MODULE.vehicles
-MODULE.name = "Trunk Storage"
-MODULE.author = "STEAM_0:1:176123778"
-MODULE.desc = "Implements storage for vehicle trunks"
-lia.util.include("sv_netcalls.lua")
