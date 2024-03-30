3bd7cab9d4f26282deb7100a7faffbe5d56fcacf
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-03-30 22:36:53 +0000

    Update shared.lua

--- 1942rp/modules/tying/submodules/tyingsearch/pim/shared.lua
+++ 1942rp/modules/tying/submodules/tyingsearch/pim/shared.lua
@@ -1,32 +1,29 @@
 ----------------------------------------------------------------------------------------------
 local MODULE = MODULE
 --------------------------------------------------------------------------------------------------------
-PIM:AddOption(
-    "Request Search",
-    {
-        runServer = true,
-        shouldShow = function(client, target) return IsHandcuffed(target) and not target.SearchRequested and not client.SearchRequested and not IsBeingSearched(target) end,
-        onRun = function(client, target)
-            if not SERVER then return end
-            net.Start("RequestSearch")
-            net.Send(target)
-            client:notify("Request to search sent.")
-            target.SearchRequested = client
-            client.SearchRequested = target
-        end
-    }
-)
+PIM:AddOption("Request Search", {
+    runServer = true,
+    shouldShow = function(client, target) return IsHandcuffed(target) and not target.SearchRequested and not client.SearchRequested and not IsBeingSearched(target) end,
+    onRun = function(client, target)
+        if not SERVER then return end
+        net.Start("RequestSearch")
+        net.Send(target)
+        client:notify("Request to search sent.")
+        target.SearchRequested = client
+        client.SearchRequested = target
+    end
+})
 
 ----------------------------------------------------------------------------------------------
-PIM:AddOption(
-    "Search",
-    {
-        runServer = true,
-        shouldShow = function(client, target) return IsHandcuffed(target) and not IsBeingSearched(target) end,
-        onRun = function(client, target)
-            if not SERVER then return end
-            MODULE:searchPlayer(client, target)
-        end
-    }
-)
+PIM:AddOption("Search", {
+    runServer = true,
+    shouldShow = function(client, target) return IsHandcuffed(target) and not IsBeingSearched(target) end,
+    onRun = function(client, target)
+        if not SERVER then return end
+        MODULE:SetupInventorySearch(client, target)
+        netstream.Start(client, "searchPly", target, target:getChar():getInv():getID())
+        client.liaSearchTarget = target
+        target:setNetVar("searcher", client)
+    end
+})
 ----------------------------------------------------------------------------------------------
