b084c41fc824639d83c4bb98480346df4b8ee532
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-26 06:22:21 +0000

    Cleanup

--- 1942rp/schema/schema.lua
+++ 1942rp/schema/schema.lua
@@ -1,9 +1,4 @@
-﻿--[[--
-    Schema configuration options.
-
-    This is meant to define the Schema's default information.
-]]
-SCHEMA.name = "1942RP"
+﻿SCHEMA.name = "1942RP"
 SCHEMA.author = "76561198312513285"
 SCHEMA.discord = "@liliaplayer"
 SCHEMA.desc = "A 1942RP Schema"
